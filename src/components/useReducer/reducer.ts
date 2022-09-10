a8095acfddf6a8027d15492b446d394e8a731935
hamham <ham@example.com>
2022-09-10 22:42:08 +0900

    commit

--- src/components/useReducer/reducer.ts
+++ src/components/useReducer/reducer.ts
@@ -1,31 +1,33 @@
-import { type } from "os";
-import { DisplayMode, NotificationDestination } from "./types";
+// import { type } from "os";
+// import { DisplayMode, NotificationDestination } from "./types";
 
 
-type Actions =
-  {
-    type: "changeDisplayMode"
-    payload: { index: number; displayMode: DisplayMode}
-  }
-  | { type: "newNotification" }
-  | { type: "updateNotification"; payload: { index: number;  value: string} }
-  | { type: "fetchAllNotification"; payload: { values: string } }
-  | { type: "deleteNotification"; payload: { index: number } }
+// type Actions =
+//   {
+//     type: "changeDisplayMode"
+//     payload: { index: number; displayMode: DisplayMode}
+//   }
+//   | { type: "newNotification" }
+//   | { type: "updateNotification"; payload: { index: number;  value: string} }
+//   | { type: "fetchAllNotification"; payload: { values: string } }
+//   | { type: "deleteNotification"; payload: { index: number } }
   
-export const reducer = (
-  state: NotificationDestination[],
-  action: Actions
-): NotificationDestination[] => {
-    switch (action.type) {
-      case "changeDisplayMode": {
-        const { index, displayMode } = action.payload
-        return state.map((notification, mindex) => {
-          return mindex === index ? {email: notification.email,displayMode} : notification
-        })
-      }
-      case "newNotification": {
-        return [...state,{displayMode:"edit",email:""}]
-      }
-      case ""
-    }
-  }
+// export const reducer = (
+//   state: NotificationDestination[],
+//   action: Actions
+// ): NotificationDestination[] => {
+//   switch (action.type) {
+//     case "changeDisplayMode": {
+//       const { index, displayMode } = action.payload
+//       return state.map((notification, mindex) => {
+//         return mindex === index ? { email: notification.email, displayMode } : notification
+//       })
+//     }
+//     case "newNotification": {
+//       return [...state, { displayMode: "edit", email: "" }]
+//     }
+
+//   }
+// }
+
+export{}
